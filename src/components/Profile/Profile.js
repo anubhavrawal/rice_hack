0a48f57322aea6a974876fb9f775c16a0913bcdb
Rithik <Jain>
2020-09-19 15:40:45 -0500

    Minor routing fix

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,10 +5,10 @@ import firebase from '../Firebase';
 class Profile extends Component {
 
 
-    firebase.firestore().collection('Users').doc(this.props.userEmail).get().then((user) => {
-            user
-        }
-    )
+    // firebase.firestore().collection('Users').doc(this.props.userEmail).get().then((user) => {
+    //         user
+    //     }
+    // )
 
     render() {
         return (
