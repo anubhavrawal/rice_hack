7920ff847d595ef7f37f85f70c0437b57f05c6ec
anubhavrawal <50759777+anubhavrawal@users.noreply.github.com>
2020-09-19 14:52:46 -0400

    data_set_correction

--- data_man.py
+++ data_man.py
@@ -1,24 +1,24 @@
 import csv
 import random
-c1 = []
-c2 = []
-c3 = []
-c4 = []
+BMI = []
+parents = []
+age = []
+outcome = []
 
 #Open for reading
 with open('diabetes.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
-        c1.append(row[0])
-        c2.append(random.randint(0, 1))
-        c3.append(row[2])
-        c4.append(row[3])
+        BMI.append(row[0])
+        parents.append(random.randint(0, 1))
+        age.append(row[2])
+        outcome.append(row[3])
         
 # Writing to file 
 with open("diabetes2.csv", "w") as file1:
     employee_writer = csv.writer(file1, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for row in range(len(c1)):
-        employee_writer.writerow([ c1[row], c2[row], c3[row], c4[row] ])
+    for row in range(len(BMI)):
+        employee_writer.writerow([ BMI[row], parents[row], age[row], outcome[row] ])
 
         
         
--- load.py
+++ load.py
@@ -2,7 +2,7 @@ from numpy import loadtxt
 from tensorflow import keras
 
 # load the dataset
-dataset = loadtxt('diabetes.csv', delimiter=',')
+dataset = loadtxt('diabetes2.csv', delimiter=',')
 # split into input (X) and output (y) variables
 X = dataset[:,0:3]
 y = dataset[:,3]
@@ -11,5 +11,5 @@ model = keras.models.load_model('model/')
 
 # make probability predictions with the model
 predictions = model.predict_classes(X)
-for i in range(20):
+for i in range(70):
 	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
